407fb3d5cd2304043ea4a849411bdbb4d715feb9
Andrii <andriikosanyak4@gmail.com>
2021-02-25 12:59:56 +0200

    submit button validation

--- barbershop/src/components/Registration/Registration.js
+++ barbershop/src/components/Registration/Registration.js
@@ -12,9 +12,16 @@ function Registration(props) {
   const [user, setUser] = useState({
     name: '', password: '', phoneNumber: '', email: '',
   });
+  const [passwords, setPasswords] = useState({ password: '', confirmPassword: '' });
+
   const [isEmailAvailable, setIsEmailAvailable] = useState(true);
   const [isPhoneAvailable, setIsPhoneAvailable] = useState(true);
-  const [passwords, setPasswords] = useState({ password: '', confirmPassword: '' });
+  const [isError, setIsError] = useState(null);
+  const [checked, setChecked] = useState(false);
+
+  const {
+    name, password, email, phoneNumber,
+  } = user;
 
   const availabilityChecker = async (type, value) => {
     const request = {
@@ -62,6 +69,7 @@ function Registration(props) {
             error={errors.INVALID_NAME}
             setUser={setUser}
             user={user}
+            setIsError={setIsError}
           />
         </Form.Group>
 
@@ -77,6 +85,7 @@ function Registration(props) {
             isEmailAvailable={isEmailAvailable}
             setUser={setUser}
             user={user}
+            setIsError={setIsError}
           />
         </Form.Group>
 
@@ -92,6 +101,7 @@ function Registration(props) {
             isPhoneAvailable={isPhoneAvailable}
             setUser={setUser}
             user={user}
+            setIsError={setIsError}
           />
         </Form.Group>
 
@@ -107,6 +117,7 @@ function Registration(props) {
             setPasswords={setPasswords}
             setUser={setUser}
             user={user}
+            setIsError={setIsError}
           />
         </Form.Group>
 
@@ -118,28 +129,40 @@ function Registration(props) {
             placeholder="Повторіть пароль"
             passwords={passwords}
             setPasswords={setPasswords}
+            setIsError={setIsError}
           />
         </Form.Group>
 
         <Form.Group controlId="formBasicCheckbox">
           <Form.Check
             type="checkbox"
+            onChange={() => setChecked(!checked)}
             label={(
-              <label>
-                Погодитись з
+              <span>
+                <span>Погодитись з </span>
                 <button
                   id="link-button"
+                  type="button"
                   onClick={() => props.history.push('/user-agreement')}
                 >
                   користувацькою
                   угодою
                 </button>
-              </label>
+              </span>
 )}
+
           />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        <Button
+          variant="primary"
+          type="submit"
+          disabled={
+            isError
+            || !(name && password && phoneNumber && email && passwords.confirmPassword)
+            || !checked
+          }
+        >
           Зареєструватись
         </Button>
       </Form>
